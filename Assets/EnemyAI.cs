36627b5d328b3769f4282ced83a2eca189a12b0a
affandhankwala <49345490+affandhankwala@users.noreply.github.com>
2022-10-02 12:58:17 -0700

    Add files via upload

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public GameObject player;
+    private float speed = 5f;
+
+    private void Update()
+    {
+        if (Vector2.Distance(transform.position, player.transform.position) > 0.7f)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, step);
+        }
+    }
+}
